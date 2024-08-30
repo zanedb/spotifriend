b1617c952ec715ac45b7907fe7ff2f3dfc65da79
Zane <github@zane.sh>
2024-08-29 17:07:45 -0700

    Release v1.4.1

--- spotifriend/Views/SettingsView.swift
+++ spotifriend/Views/SettingsView.swift
@@ -44,7 +44,7 @@ struct SettingsView: View {
                     HStack {
                         Text("Version")
                         Spacer()
-                        Text("2024.06 (\(SSApp.version))")
+                        Text("2024.08 (\(SSApp.version))")
                             .foregroundColor(.gray)
                     }
                     
